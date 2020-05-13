c01381432f3955f72148e552bfcf0e8477bb94bc
rgyan12 <rgyan@rocketmail.com>
2020-05-13 20:24:00 +0100

    updated the work page

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,12 @@
       <router-link to="/about">About</router-link>
     </div>
     -->
-    
+    <transition
+        name="fade"
+        mode="out-in"
+      >
     <router-view />
+     </transition>
 
     <Footer-component></Footer-component>
     <SocialFooter></SocialFooter>
@@ -47,7 +51,17 @@ body {
 }
 
 
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.1s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
 
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
 
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,10 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior () {
+  return { x: 0, y: 0 }
+}
 });
 
 router.beforeEach((to, from, next) => {
